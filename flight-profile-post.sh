d81c391526bc101bb59a2dfe9e7fdc930d3270ee
Stu Franks <stu@stuts.uk>
2023-01-05 10:31:03 +0000

    Remove post/update tasks

--- flight-profile-post.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-ansible-playbook -i $INVFILE --extra-vars="cluster_name=$CLUSTERNAME default_username=$DEFAULT_USERNAME compute_ip_range=$COMPUTE_IP_RANGE hunter_hosts=$HUNTER_HOSTS" --tags update $RUN_ENV/openflight-kubernetes-multinode/main.yml
