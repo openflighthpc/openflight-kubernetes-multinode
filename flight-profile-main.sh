f0da8be2fa5fe6f853a6dd682ceaa7fb07ebfb05
Stu Franks <stu@stuts.uk>
2023-01-25 12:34:41 +0000

    Fix typo

--- flight-profile-main.sh
+++ flight-profile-main.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ansible-playbook -i $INVFILE --limit $NODE --extra-vars="cluster_name=$CLUSTERNAME nfs_server=$NFS_SERVER default_username=$DEFAULT_USERNAME default_user_password=$DEFAULT_PASSWORD access_host=$ACCESS_HOS compute_ip_range=$COMPUTE_IP_RANGE hunter_hosts=$HUNTER_HOSTS" $RUN_ENV/openflight-kubernetes-multinode/main.yml
+ansible-playbook -i $INVFILE --limit $NODE --extra-vars="cluster_name=$CLUSTERNAME nfs_server=$NFS_SERVER default_username=$DEFAULT_USERNAME default_user_password=$DEFAULT_PASSWORD access_host=$ACCESS_HOST compute_ip_range=$COMPUTE_IP_RANGE hunter_hosts=$HUNTER_HOSTS" $RUN_ENV/openflight-kubernetes-multinode/main.yml
